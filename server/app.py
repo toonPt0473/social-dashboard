b6bfa87fc7ca21e1a83e75938704c7102abf2af8
ttoonoott <rimb9pkmink@gmail.com>
2020-02-16 15:04:24 +0700

    work with docker-compose

--- server/app.py
+++ server/app.py
@@ -13,20 +13,5 @@ api = Api(app, prefix='/api/')
 
 api.add_resource(data.Controller, '/data')
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-
-def callback(ch, method, properties, body):
-  print(" [x] Received %r" % body)
-
-
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
-
 if __name__ == "__main__":
   app.run(debug=True, port=3000, host="0.0.0.0")
--- server/controller/data.py
+++ server/controller/data.py
@@ -4,16 +4,10 @@ import pika
 
 
 class Controller(Resource):
-  # def __init__(self):
   def post(self):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
-
     channel.queue_declare(queue='hello')
-
     channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
     print(" [x] Sent 'Hello World!'")
     connection.close()
-    json_data = request.get_json(force=True)
-    print(json_data)
-    return 
