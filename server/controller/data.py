64edca4cba150e2f5a182edba208e02a5189a626
ttoonoott <rimb9pkmink@gmail.com>
2020-02-16 22:20:20 +0700

    add pandas for make dataframe

--- server/controller/data.py
+++ server/controller/data.py
@@ -1,13 +1,14 @@
 from flask_restful import Resource
 from flask import request
 import pika
-
+import json
 
 class Controller(Resource):
   def post(self):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    body = request.get_json()
+    body = json.dumps(body)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    print(" [x] Sent 'Hello World!'")
+    channel.queue_declare(queue='addNewCsv')
+    channel.basic_publish(exchange='', routing_key='addNewCsv', body=body)
     connection.close()
--- worker/app.py
+++ worker/app.py
@@ -1,21 +1,73 @@
 import pika
 import time
-import logging
-import time
-logging.basicConfig()
+import json
+import pandas as pd
+import re
+import os
+from datetime import datetime
 
 time.sleep(10)
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='addNewCsv')
+
+def generate_dataframe_from_file(filename):
+  type_word = ['tweet', 'post', 'comment', 'reply', 'reply-comment', 'reply-reply-comment', 'news']
+  def repl_func_start_content(matchobj):
+      return f'''""'''
+  def repl_func_end_content(matchobj):
+      return f'"{matchobj.group()}'
+  def repl_func_quote(matchobj):
+      return f'""'
+  def repl_func_content_comma(matchobj):
+      return matchobj.group().replace(',', '')
+  def repl_func_name(matchobj):
+      text = matchobj.group().split(',')
+      return text[0] + ',"' + ','.join(text[1:]) + '"'
+  def repl_func_newline_content(matchobj):
+      return re.sub(r'''\n''', '${newline}', matchobj.group())
+
+  if (os.path.exists('newdata.csv')):
+    os.remove("newdata.csv")
+  w = open("newdata.csv", "a+")
+  with open(filename) as infile:
+    for line in infile:
+      line = re.sub(r'''"''', repl_func_quote, line)
+      line = re.sub(r"""'""", repl_func_quote, line)
+      for t in type_word:
+          start_message_regex = r',' + t + r','
+          line = re.sub(start_message_regex, f',{t},"', line)
+      line = re.sub(r''',\d{4}-\d{2}-\d{1,2}\s\d{2}:\d{2}:\d{2}''', repl_func_end_content, line)
+      line = re.sub(r'(?<=,twitter,)(.*)(?=\n)', repl_func_name, line)
+      line = re.sub(r'(?<=,facebook,)(.*)(?=\n)', repl_func_name, line)
+      line = re.sub(r'(?<=,website,)(.*)(?=\n)', repl_func_name, line)
+      line = re.sub(r'(?<=,instagram,)(.*)(?=\n)', repl_func_name, line)
+      line = re.sub(r'(?<=,youtube,)(.*)(?=\n)', repl_func_name, line)
+      line = re.sub(r'(?<=,pantip,)(.*)(?=\n)', repl_func_name, line)
+      w.write(line)
+  w.close()
+  print('===============================================')
+  f = open('newdata.csv')
+  contents = f.read()
+  f.close()
+  contents = re.sub(r'''"([^"]*)"''', repl_func_newline_content, contents)
+  f = open('newdata.csv', 'w')
+  f.write(contents)
+  f.close()
+  print('end')
+  csv = pd.read_csv('newdata.csv', error_bad_lines=False)
+  return csv
 
 
 def callback(ch, method, properties, body):
-  print(" [x] Received %r" % body)
+  filename = 'rawdata.csv'
+  csv = generate_dataframe_from_file(filename)
+
 
 channel.basic_consume(
-  queue='hello', on_message_callback=callback, auto_ack=True)
+  queue='addNewCsv', on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
+
